a0a5d42e4571deba1084106c318d861a4293632d
Dalibor Bilický <d.bilicky@gmail.com>
2025-10-26 11:09:58 +0100

    Updated jdtls config do to error

--- nvim/lua/jdtls/jdtls_setup.lua
+++ nvim/lua/jdtls/jdtls_setup.lua
@@ -2,12 +2,25 @@ local M = {}
 
 function M:setup()
 	local project_name = vim.fn.fnamemodify(vim.fn.getcwd(), ":p:h:t")
-	local workspace_dir = vim.fn.stdpath("data")
-		.. package.config:sub(1, 1)
-		.. "jdtls-workspace"
-		.. package.config:sub(1, 1)
-		.. project_name
+
 	local os_name = vim.loop.os_uname().sysname
+	local os_config = os_name == "Linux" and "linux"
+		or os_name == "Darwin" and "mac"
+		or os_name == "Windows_NT" and "win"
+		or error("Unsupported OS: " .. os_name)
+
+	local mason_path = vim.fn.stdpath("data") .. "/mason/packages/jdtls"
+	local launcher_jar = mason_path .. "/plugins/org.eclipse.equinox.launcher_1.7.0.v20250519-0528.jar"
+	local config_dir = mason_path .. "/config_" .. os_config
+	local workspace_dir = vim.fn.stdpath("data") .. "/jdtls-workspace/" .. project_name
+
+	vim.fn.mkdir(workspace_dir, "p")
+
+	if vim.fn.filereadable(launcher_jar) == 0 then
+		vim.notify("JDT LS launcher JAR not found: " .. launcher_jar, vim.log.levels.ERROR)
+		return
+	end
+
 	local config = {
 		cmd = {
 			"java",
@@ -17,34 +30,10 @@ function M:setup()
 			"-Dlog.protocol=true",
 			"-Dlog.level=ALL",
 			"-Xmx1g",
-			"--add-modules=ALL-SYSTEM",
-			"--add-opens",
-			"java.base/java.util=ALL-UNNAMED",
-			"--add-opens",
-			"java.base/java.lang=ALL-UNNAMED",
 			"-jar",
-			vim.fn.stdpath("data")
-				.. package.config:sub(1, 1)
-				.. "mason"
-				.. package.config:sub(1, 1)
-				.. "packages"
-				.. package.config:sub(1, 1)
-				.. "jdtls"
-				.. package.config:sub(1, 1)
-				.. "plugins"
-				.. package.config:sub(1, 1)
-				.. "org.eclipse.equinox.launcher_1.7.0.v20250519-0528.jar",
+			launcher_jar,
 			"-configuration",
-			vim.fn.stdpath("data")
-				.. package.config:sub(1, 1)
-				.. "mason"
-				.. package.config:sub(1, 1)
-				.. "packages"
-				.. package.config:sub(1, 1)
-				.. "jdtls"
-				.. package.config:sub(1, 1)
-				.. "config_"
-				.. (os_name == "Windows_NT" and "win" or os_name == "Linux" and "linux" or "mac"),
+			config_dir,
 			"-data",
 			workspace_dir,
 		},
@@ -56,6 +45,7 @@ function M:setup()
 			bundles = {},
 		},
 	}
+
 	require("jdtls").start_or_attach(config)
 end
 
